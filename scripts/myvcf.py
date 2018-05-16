c97f0947b539bbaaf9c1d980c1d8e2f506d368d5
Amie Radenbaugh <amie.radenbaugh@gmail.com>
2018-05-16 14:32:17 -0700

    Minor code updates
        - Stopped using python reserved words as var names
    
        - Removed extra_headers that were never used

--- scripts/myvcf.py
+++ scripts/myvcf.py
@@ -29,57 +29,58 @@ class VCFFormatError(Exception):
         Exception.__init__(self, text)
 
 class Filter:
-    def __init__(self, id, description):
-        self.id = id
-        self.description = description
+    def __init__(self, anId, aDescription):
+        self.id = anId
+        self.description = aDescription
 
 class Info:
-    def __init__(self, id, number, type, description):
-        self.id = id
-        self.number = number
-        self.type = type
-        self.description = description
+    def __init__(self, anId, aNumber, aType, aDescription):
+        self.id = anId
+        self.number = aNumber
+        self.type = aType
+        self.description = aDescription
         
     def __str__(self):
         return '##INFO=<ID=%s,Number=%s,Type=%s,Description="%s">' % (self.id, self.number, self.type, self.description)
 
+
 class Sample:
-    def __init__(self, id, individual, description, file, platform, source, accession=None):
-        self.id = id
-        self.individual = individual
-        self.description = description
-        self.file = file
-        self.platform = platform
-        self.source = source
-        self.accession = accession
+    def __init__(self, anId, anIndividual, aDescription, aFile, aPlatform, aSource, anAccession=None):
+        self.id = anId
+        self.individual = anIndividual
+        self.description = aDescription
+        self.file = aFile
+        self.platform = aPlatform
+        self.source = aSource
+        self.accession = anAccession
     
-    def __init__(self, sampledict):
-        if "ID" in sampledict:
-            self.id = sampledict["ID"]
+    def __init__(self, aSampleDict):
+        if "ID" in aSampleDict:
+            self.id = aSampleDict["ID"]
         else:
             self.id = ""
-        if "Individual" in sampledict:
-            self.individual = sampledict["Individual"].strip('"')
+        if "Individual" in aSampleDict:
+            self.individual = aSampleDict["Individual"].strip('"')
         else:
             self.individual = ""
-        if "Description" in sampledict:
-            self.description = sampledict["Description"].strip('"')
+        if "Description" in aSampleDict:
+            self.description = aSampleDict["Description"].strip('"')
         else:
             self.description=""
-        if "File" in sampledict:
-            self.file = sampledict["File"].strip('"')
+        if "File" in aSampleDict:
+            self.file = aSampleDict["File"].strip('"')
         else:
             self.file = ""
-        if "Platform" in sampledict:
-            self.platform = sampledict["Platform"].strip('"')
+        if "Platform" in aSampleDict:
+            self.platform = aSampleDict["Platform"].strip('"')
         else:
             self.platform = ""
-        if "Source" in sampledict:
-            self.source = sampledict["Source"].strip('"')
+        if "Source" in aSampleDict:
+            self.source = aSampleDict["Source"].strip('"')
         else:
             self.source = ""
-        if "Accession" in sampledict:
-            self.accession = sampledict["Accession"].strip('"')
+        if "Accession" in aSampleDict:
+            self.accession = aSampleDict["Accession"].strip('"')
         else:
             self.accession = ""
 
@@ -89,52 +90,56 @@ class Sample:
         else:
             return '##SAMPLE=<ID=%s,Individual="%s",Description="%s",File="%s",Platform="%s",Source="%s">' % (self.id, self.individual, self.description, self.file, self.platform, self.source)
 
-def init_from_match(id, individual, description, file, platform, source, accession=None):
+
+def init_from_match(anId, anIndividual, aDescription, aFile, aPlatform, aSource, anAccession=None):
     initdict = {}
-    initdict["ID"] = id
-    initdict["Individual"] = individual
-    initdict["Description"] = description
-    initdict["File"] = file
-    initdict["Platform"] = platform
-    initdict["Source"] = source
-    if accession:
-        initdict["Accession"] = accession
+    initdict["ID"] = anId
+    initdict["Individual"] = anIndividual
+    initdict["Description"] = aDescription
+    initdict["File"] = aFile
+    initdict["Platform"] = aPlatform
+    initdict["Source"] = aSource
+    if anAccession:
+        initdict["Accession"] = anAccession
     return Sample(initdict)
 
 
-def parse_info(info):
+def parse_info(anInfoField):
     info_dict = OrderedDict()
-    for i in info.split(";"):
-        if "=" in i:
-            id,value = i.split("=")
-            info_dict[id]=value
+    for item in anInfoField.split(";"):
+        if "=" in item:
+            (tag, value) = item.split("=")
+            info_dict[tag] = value
         else:
-            info_dict[i]=True
+            info_dict[item] = True
     return info_dict
 
-def format_info(info_dict):
+
+def format_info(anInfoDict):
     line_list = []
-    for key, value in info_dict.items():
+    for key, value in anInfoDict.items():
         if value is True:
             line_list.append(key)
         else:
             line_list.append("%s=%s" % (key, value))
     return ";".join(line_list)
     
+
 class Data:
-    def __init__(self, chrom, pos, id, ref, alt, qual, filter, info, genotype_data = None, extra_headers = None):
-        self.chrom = chrom
-        self.pos = int(pos)
-        self.id = id
-        self.ref = ref
-        self.alt = alt.split(",")
-        self.qual = qual
-        self.filter = filter.split(";")
-        self.info = parse_info(info)
-        self.genotype = genotype_data
+    def __init__(self, aChrom, aPos, anId, aRef, anAltField, aQual, aFilterField, anInfoField, aGenotypeData = None):
+        self.chrom = aChrom
+        self.pos = int(aPos)
+        self.id = anId
+        self.ref = aRef
+        self.alt = anAltField.split(",")
+        self.qual = aQual
+        self.filter = aFilterField.split(";")
+        self.info = parse_info(anInfoField)
+        self.genotype = aGenotypeData
 
     def __str__(self):
         return "\t".join(map(str, [self.chrom, self.pos, self.id, self.ref, ",".join(self.alt), self.qual, ";".join(self.filter), format_info(self.info), "\t".join(self.genotype)]))
+
 
 class VCF:
     def __init__(self):
@@ -145,70 +150,74 @@ class VCF:
         self.headers = []
         self.data = []
 
-    def make_info(self, value):
+
+    def make_info(self, aValue):
         #make re match spec
-        match = re.match(r"""<ID=(.*),Number=(.*),Type=(.*),Description=['"](.*)['"]""", value)
+        match = re.match(r"""<ID=(.*),Number=(.*),Type=(.*),Description=['"](.*)['"]""", aValue)
         if match:
             return Info(*match.groups())
         else:
-            raise VCFFormatError("Improperly formatted INFO metadata: " + value)
+            raise VCFFormatError("Improperly formatted INFO metadata: " + aValue)
             
-    def make_sample(self, value):
-        match = re.match(r"""<ID=(.*),Individual="(.*)",Description="(.*)",File="(.*)",Platform="(.*)",Source="(.*)">""", value)
+
+    def make_sample(self, aValue):
+        match = re.match(r"""<ID=(.*),Individual="(.*)",Description="(.*)",File="(.*)",Platform="(.*)",Source="(.*)">""", aValue)
         if match:
             return init_from_match(*match.groups())
         else:
-            match = re.match(r"""<ID=(.*),Individual="(.*)",Description="(.*)",File="(.*)",Platform="(.*)",Source="(.*)",Accession=(.*)>""", value)
+            match = re.match(r"""<ID=(.*),Individual="(.*)",Description="(.*)",File="(.*)",Platform="(.*)",Source="(.*)",Accession=(.*)>""", aValue)
             if match:
                 return Sample(*match.groups())
             else:
                 sampledict = {}
-                for pair in value.strip("<>").split(","):
-                    key, value = pair.split("=")
-                    sampledict[key] = value
+                for pair in aValue.strip("<>").split(","):
+                    key, val = pair.split("=")
+                    sampledict[key] = val
                 return Sample(sampledict)
                 
+        raise VCFFormatError("Improperly formatted SAMPLE metadata: " + aValue)
         
-        raise VCFFormatError("Improperly formatted SAMPLE metadata: " + value)
         
-    def add_info(self, value):
-        self.infos.append(self.make_info(value))
+    def add_info(self, aValue):
+        self.infos.append(self.make_info(aValue))
     
-    def add_filter(self, value):
+    def add_filter(self, aValue):
         #make re match spec
-        match = re.match(r"""<ID=(.*),Description=['"](.*)['"]>""", value)
+        match = re.match(r"""<ID=(.*),Description=['"](.*)['"]>""", aValue)
         if match:
             self.filters.append(Filter(*match.groups()))
         else:
-            raise VCFFormatError("Improperly formatted FILTER metadata: " + value)
-        #self.filters.append(Filter(values[0], values[1]))
+            raise VCFFormatError("Improperly formatted FILTER metadata: " + aValue)
 
-    def set_headers(self, header_list):
-        self.headers = header_list
 
-    def make_data(self, data_list):
-        baseData = data_list[:8]
-        genotypeData = data_list[8:]
-        data = baseData+[genotypeData]+[self.headers[8:]]
+    def set_headers(self, aHeaderList):
+        self.headers = aHeaderList
+
+
+    def make_data(self, aDataList):
+        baseData = aDataList[:8]
+        genotypeData = aDataList[8:]
+        data = baseData + [genotypeData]
         return Data(*data)
-        
+
+
     def add_data(self, data_list):
         self.data.append(self.make_data(data_list))
 
 
-def make_accession(filename):
-    file = open(filename, 'r')
+def make_accession(aFilename):
+    accessionFile = open(aFilename, 'r')
     
     #ignore first line
-    file.readline()
+    accessionFile.readline()
     
     accession = {}
-    for line in file:
+    for line in accessionFile:
         data = line.strip().split(',')
         srs = data[3]
         match = re.search(r'TCGA-\w{2}-\w{4}-\w{3}-\w{3}-\w{4}-\w{2}', data[4])
         if match:
-            file = match.group()
-            accession[file] = srs
+            filename = match.group()
+            accession[filename] = srs
     return accession
 
